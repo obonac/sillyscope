436e8c15dafcfe2e9fb891461b9cb1378965e788
James Halliday <mail@substack.net>
2013-06-05 16:48:00 -0700

    log base 2 scale

--- browser/main.js
+++ browser/main.js
@@ -4,7 +4,7 @@ var through = require('through');
 
 var canvas = document.createElement('canvas');
 canvas.setAttribute('width', window.innerWidth);
-canvas.setAttribute('height', 5500);
+canvas.setAttribute('height', window.innerHeight);
 document.body.appendChild(canvas);
 
 var ctx = canvas.getContext('2d');
@@ -13,12 +13,16 @@ var pos = 0;
 stream.pipe(through(function (line) {
     var rows = JSON.parse(line);
     var output = ctx.createImageData(1, canvas.height);
+    var dy = canvas.height / Math.log(canvas.height + 1) * Math.log(2);
     
     rows.forEach(function (row, i) {
         var x = clamp(Math.pow(row[1], 2));
-        var j = i * 4;
-        output.data[j] = output.data[j+1] = output.data[j+2] = x;
-        output.data[j+3] = 255;
+        var y = Math.floor(dy * Math.log(i + 1) / Math.log(2));
+        var y_ = Math.floor(dy * Math.log(i + 2) / Math.log(2));
+        for (var j = y * 4; j <= y_ * 4; j += 4) {
+            output.data[j] = output.data[j+1] = output.data[j+2] = x;
+            output.data[j+3] = 255;
+        }
     });
     
     ctx.putImageData(output, pos++ % canvas.width, 0);
